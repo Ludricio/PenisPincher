5e71402f20423dd709a5a8dffec7be1acde71054
Ludvig Rydahl <ludvig.rydahl@gmail.com>
2021-03-21 00:26:43 +0100

    Added configuring of services

--- PenisPincher.Discord/DiscordLauncher.cs
+++ PenisPincher.Discord/DiscordLauncher.cs
@@ -1,12 +1,18 @@
 ﻿using System;
 using System.Threading;
 using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using PenisPincher.Discord.Services;
+using PenisPincher.Utilities;
 
 namespace PenisPincher.Discord
 {
 
-    public class DiscordLauncher
+    public class DiscordLauncher : AsyncDisposable
     {
         private ILogger<DiscordLauncher> Logger { get; set; }
         private ManualResetEvent WaitHandle { get; }
@@ -19,6 +25,41 @@ namespace PenisPincher.Discord
         public async Task StartDiscordServicesAsync(IServiceProvider serviceProvider)
         {
 
+        }
+
+        public static void ConfigureServices(IServiceCollection services)
+        {
+            services
+                .AddSingleton(new DiscordSocketClient(new DiscordSocketConfig
+                {
+#if DEBUG
+                    LogLevel = LogSeverity.Info,
+#else
+                LogLevel = LogSeverity.Verbose,
+#endif
+                    MessageCacheSize = 1000
+                }))
+                .AddSingleton(new CommandService(new CommandServiceConfig
+                {
+#if DEBUG
+                    LogLevel = LogSeverity.Info,
+#else
+                    LogLevel = LogSeverity.Verbose,
+#endif
+                    DefaultRunMode = RunMode.Async
+                }))
+                .AddSingleton<DiscordLoggingService>()
+                .AddSingleton<CommandHandlerService>()
+                .AddSingleton<DiscordLoginService>();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if(Disposed) return;
+            if(disposing)
+            {
+                WaitHandle.Dispose();
+            }
         }
     }
 }
