0e59edf8d671d2c1da29c6544d88b984d27b10cf
Ludvig Rydahl <ludvig.rydahl@gmail.com>
2021-03-21 10:54:16 +0100

    Added repository interface

---
+++ PenisPincher.Persistence/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace PenisPincher.Persistence
+{
+    public interface IRepository<TEntity> where TEntity : class
+    {
+
+        TEntity Get(ulong id);
+
+        void Insert(TEntity entity);
+
+        void Update(TEntity entity);
+
+        void Remove(ulong id);
+
+        int Count(Expression<Func<TEntity, bool>> predicate = null);
+    }
+}
