5d8edb86d9ff1129a6d724166009c3a2aa437dfe
Ludvig Rydahl <ludvig.rydahl@gmail.com>
2021-03-21 11:03:41 +0100

    Added persistence configuration infrastructure to map entities via fluent API

---
+++ PenisPincher.Persistence/Configuration/EntityConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace PenisPincher.Persistence.Configuration
+{
+    public abstract class EntityConfiguration<TEntity> : IEntityTypeConfiguration<TEntity> where TEntity : class
+    {
+        public void Configure(EntityTypeBuilder<TEntity> builder)
+        {
+
+        }
+
+        protected abstract void Configure(EntityConfigurator<TEntity> configurator);
+    }
+}
---
+++ PenisPincher.Persistence/Configuration/EntityConfigurator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace PenisPincher.Persistence.Configuration
+{
+    public class EntityConfigurator<TEntity> where TEntity : class
+    {
+        private EntityTypeBuilder<TEntity> EntityTypeBuilder { get; }
+
+        public EntityConfigurator(EntityTypeBuilder<TEntity> entityTypeBuilder)
+        {
+            EntityTypeBuilder = entityTypeBuilder;
+        }
+
+        public EntityConfigurator<TEntity> Has(Action<EntityTypeBuilder<TEntity>> builder)
+        {
+            builder(EntityTypeBuilder);
+            return this;
+        }
+    }
+}
