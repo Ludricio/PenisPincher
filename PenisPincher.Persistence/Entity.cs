3003e230e97662d1fac48d82085cb464fe6144a4
Ludvig Rydahl <ludvig.rydahl@gmail.com>
2021-03-25 10:06:46 +0100

    Added Entity base class with default entity properties

---
+++ PenisPincher.Persistence/Entity.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PenisPincher.Persistence
+{
+    public interface IEntity
+    {
+        object Id { get; set; }
+        DateTime CreatedDate { get; set; }
+        DateTime? ModifiedDate { get; set; }
+        string CreatedBy { get; set; }
+        string ModifiedBy { get; set; }
+        public byte[] Version { get; set; }
+    }
+
+    public interface IEntity<T> : IEntity
+    {
+        new T Id { get; set; }
+    }
+
+    public abstract class Entity<T> : IEntity<T>
+    {
+        private Entity()
+        {
+            CreatedDate = DateTime.UtcNow;
+        }
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public T Id { get; set; }
+
+        object IEntity.Id
+        {
+            get => Id;
+            set => Id = (T)value;
+        }
+
+        [DataType(DataType.DateTime)]
+        public DateTime CreatedDate { get; set; }
+
+        [DataType(DataType.DateTime)]
+        public DateTime? ModifiedDate { get; set; }
+
+        public string CreatedBy { get; set; }
+
+        public string ModifiedBy { get; set; }
+
+        [Timestamp]
+        public byte[] Version { get; set; }
+    }
+}
