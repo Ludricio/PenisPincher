7c068aa79f9aa3a072ea56ceea0cea854d478482
Ludvig Rydahl <ludvig.rydahl@gmail.com>
2021-03-25 16:43:10 +0100

    Added tracking of currently monitored roles, and ability to add/remove monitored roles from service

--- PenisPincher.Discord/Services/ReactionRoleService.cs
+++ PenisPincher.Discord/Services/ReactionRoleService.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,6 +7,7 @@ using System.Threading.Tasks;
 using Discord;
 using Discord.WebSocket;
 using Microsoft.Extensions.Logging;
+using PenisPincher.Core.Models;
 
 namespace PenisPincher.Discord.Services
 {
@@ -15,10 +17,13 @@ namespace PenisPincher.Discord.Services
 
         private ILogger<ReactionRoleService> Logger { get; }
 
+        private ConcurrentDictionary<ulong, ConcurrentDictionary<string, ReactionRole>> MonitoredReactions { get; }
+
         public ReactionRoleService(DiscordSocketClient discordClient, ILogger<ReactionRoleService> logger)
         {
             DiscordClient = discordClient;
             Logger = logger;
+            MonitoredReactions = new ConcurrentDictionary<ulong, ConcurrentDictionary<string, ReactionRole>>();
 
             DiscordClient.ReactionAdded += OnReactionAdded;
             DiscordClient.ReactionRemoved += OnReactionRemoved;
@@ -33,6 +38,26 @@ namespace PenisPincher.Discord.Services
         private async Task OnReactionRemoved(Cacheable<IUserMessage, ulong> cache, ISocketMessageChannel smc, SocketReaction sr)
         {
             throw new NotImplementedException();
+        }
+
+        public void AddMonitoredReaction(ReactionRole reactionRole)
+        {
+            if(!MonitoredReactions.TryGetValue(reactionRole.Id, out var reactionSet))
+            {
+                reactionSet = new ConcurrentDictionary<string, ReactionRole>();
+                MonitoredReactions.TryAdd(reactionRole.Id, reactionSet);
+                //TODO add bot's reaction to message
+            }
+
+            reactionSet.GetOrAdd(reactionRole.EmoteName, reactionRole);
+        }
+
+        public void RemoveMonitoredReaction(ReactionRole reactionRole)
+        {
+            if(!MonitoredReactions.TryGetValue(reactionRole.Id, out var reactionSet)) return;
+
+            reactionSet.TryRemove(reactionRole.EmoteName, out _);
+            //TODO remove bot's reaction from message
         }
     }
 }
