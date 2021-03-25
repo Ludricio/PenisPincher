e299d29d1167ee2e90255540ca22696a2c8aabc9
Ludvig Rydahl <ludvig.rydahl@gmail.com>
2021-03-25 10:06:18 +0100

    Added async and range varieties

--- PenisPincher.Persistence/IRepository.cs
+++ PenisPincher.Persistence/IRepository.cs
@@ -1,19 +1,31 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq.Expressions;
+using System.Threading.Tasks;
 
 namespace PenisPincher.Persistence
 {
-    public interface IRepository<TEntity> where TEntity : class
+    public interface IRepository<TEntity> where TEntity : class, IEntity<ulong>
     {
 
-        TEntity Get(ulong id);
 
-        void Insert(TEntity entity);
+        IEnumerable<TEntity> Get(Expression<Func<TEntity, bool>> predicate = null);
+        TEntity Get(ulong id);
+        IAsyncEnumerable<TEntity> GetAsync(Expression<Func<TEntity, bool>> predicate = null);
+        Task<TEntity> GetAsync(ulong id);
+
+        void Add(TEntity entity);
+        void AddRange(IEnumerable<TEntity> entities);
+        Task AddAsync(TEntity entity);
+        Task AddRangeAsync(IEnumerable<TEntity> entities);
 
         void Update(TEntity entity);
+        void UpdateRange(IEnumerable<TEntity> entities);
 
-        void Remove(ulong id);
+        void Remove(TEntity entity);
+        void RemoveRange(IEnumerable<TEntity> entities);
 
         int Count(Expression<Func<TEntity, bool>> predicate = null);
+        Task<int> CountAsync(Expression<Func<TEntity, bool>> predicate = null);
     }
 }
