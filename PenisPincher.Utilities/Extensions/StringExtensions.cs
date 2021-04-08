47f1ff0650e41b05aa821dfcad32d4f7a1122ab2
Ludvig Rydahl <ludvig.rydahl@gmail.com>
2021-04-08 10:21:49 +0200

    Removed unnecessary nullcheck

--- PenisPincher.Utilities/Extensions/StringExtensions.cs
+++ PenisPincher.Utilities/Extensions/StringExtensions.cs
@@ -18,15 +18,13 @@ namespace PenisPincher.Utilities.Extensions
 
         public static void ThrowIfNullOrEmpty(this string @this, string paramName)
         {
-            @this.ThrowIfNull(paramName);
-            if (@this.IsNullOrEmpty()) throw new ArgumentException("Argument cannot be empty", paramName);
+            if (@this.IsNullOrEmpty()) throw new ArgumentException("Argument cannot be null or empty", paramName);
         }
 
         public static void ThrowIfNullOrWhitespace(this string @this, string paramName)
         {
-            @this.ThrowIfNull(paramName);
             if (@this.IsNullOrWhitespace())
-                throw new ArgumentException("Argument cannot consist only of whitespace", paramName);
+                throw new ArgumentException("Argument cannot be null or whitespace only", paramName);
         }
 
         public static bool EqualsIgnoreCase(this string @this, string other)
