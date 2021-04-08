8f617e408d1e710aa787a2e8d86901f22128283b
Ludvig Rydahl <ludvig.rydahl@gmail.com>
2021-04-08 12:06:24 +0200

    Implemented builder for building embedded discord server log messages and redid LogginExtensions for discord server logging to only use the builder.

---
+++ PenisPincher.Discord/DiscordServerLogBuilder.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using Discord;
+using JetBrains.Annotations;
+
+namespace PenisPincher.Discord
+{
+    public class DiscordServerLogBuilder : IDiscordServerLogBuilder, IDiscordServerLogLocationBuilder, IDiscordServerLogMessageBuilder, IDiscordServerLogReasonBuilder, IDiscordServerLogEmbedBuilder
+    {
+        private Color Color { get; }
+        private string LogTitle { get; set; }
+        private Tuple<string, object[]> Location { get; set; }
+        private Tuple<string, object[]> Message { get; set; }
+        private Tuple<string, object[]> Reason { get; set; }
+
+        public DiscordServerLogBuilder(Color color)
+        {
+            Color = color;
+            LogTitle = null;
+            Location = null;
+            Reason = null;
+            Message = null;
+        }
+
+        public Embed Build()
+        {
+            var builder = new EmbedBuilder()
+                .WithTitle(LogTitle)
+                .WithColor(Color)
+                .WithFields(ConvertToEmbedFieldBuilders())
+                .WithCurrentTimestamp();
+            return builder.Build();
+        }
+
+        private IEnumerable<EmbedFieldBuilder> ConvertToEmbedFieldBuilders()
+        {
+            return new EmbedFieldBuilder[]
+            {
+                new()
+                {
+                    Name = nameof(Location),
+                    Value = string.Format(Location.Item1, Location.Item2)
+                },
+                new()
+                {
+                    Name = nameof(Reason),
+                    Value = string.Format(Reason.Item1, Reason.Item2)
+                },
+                new()
+                {
+                    Name = nameof(Message),
+                    Value = string.Format(Message.Item1, Message.Item2)
+                },
+            };
+        }
+
+        public IDiscordServerLogLocationBuilder WithTitle(string errorTitle)
+        {
+            LogTitle = errorTitle;
+            return this;
+        }
+
+        IDiscordServerLogMessageBuilder IDiscordServerLogLocationBuilder.WithLocation(string location, params object[] parameters)
+        {
+            Location = Tuple.Create(location, parameters);
+            return this;
+        }
+
+        IDiscordServerLogReasonBuilder IDiscordServerLogMessageBuilder.WithMessage(string message, params object[] parameters)
+        {
+            Message = Tuple.Create(message, parameters);
+            return this;
+        }
+
+        IDiscordServerLogEmbedBuilder IDiscordServerLogReasonBuilder.WithReason(string reason, params object[] parameters)
+        {
+            Reason = Tuple.Create(reason, parameters);
+            return this;
+        }
+    }
+
+    public interface IDiscordServerLogBuilder
+    {
+        IDiscordServerLogLocationBuilder WithTitle(string errorTitle);
+        Embed Build();
+    }
+
+    public interface IDiscordServerLogLocationBuilder
+    {
+        [StringFormatMethod("location")]
+        IDiscordServerLogMessageBuilder WithLocation(string location, params object[] parameters);
+    }
+
+    public interface IDiscordServerLogMessageBuilder
+    {
+        [StringFormatMethod("message")]
+        IDiscordServerLogReasonBuilder WithMessage(string message, params object[] parameters);
+    }
+
+    public interface IDiscordServerLogReasonBuilder
+    {
+        [StringFormatMethod("reason")]
+        IDiscordServerLogEmbedBuilder WithReason(string reason, params object[] parameters);
+    }
+
+    public interface IDiscordServerLogEmbedBuilder
+    {
+        Embed Build();
+    }
+
+
+
+}
--- PenisPincher.Discord/Extensions/LoggingExtensions.cs
+++ PenisPincher.Discord/Extensions/LoggingExtensions.cs
@@ -8,35 +8,35 @@ namespace PenisPincher.Discord.Extensions
 {
     public static class LoggingExtensions
     {
-        public static async Task LogToServerAsync(this SocketGuild @this, DiscordServerLogLevel logLevel, DiscordServerConfiguration serverConfig, string message = "", EmbedBuilder embedBuilder = null)
+        public static async Task LogToServerAsync(this SocketGuild @this, DiscordServerLogLevel logLevel, DiscordServerConfiguration serverConfig, IDiscordServerLogBuilder embedBuilder)
         {
             if(!serverConfig.LogToServer || logLevel < serverConfig.LogLevel) return;
 
             var channel = @this.GetTextChannel(serverConfig.ServerLogChannelId);
             if (channel is null) return;
-
-            await channel.SendMessageAsync(message, false, embedBuilder?.Build());
+            
+            await channel.SendMessageAsync("", false, embedBuilder.Build());
         }
 
         public static async Task LogDebugToServerAsync(this SocketGuild @this,
                                                              DiscordServerConfiguration serverConfig,
-                                                             string message = "", EmbedBuilder embedBuilder = null)
+                                                             IDiscordServerLogBuilder embedBuilder)
         {
-            await LogToServerAsync(@this, DiscordServerLogLevel.Verbose, serverConfig, message, embedBuilder);
+            await LogToServerAsync(@this, DiscordServerLogLevel.Verbose, serverConfig, embedBuilder);
         }
 
         public static async Task LogInformationToServerAsync(this SocketGuild @this,
                                                              DiscordServerConfiguration serverConfig,
-                                                             string message = "", EmbedBuilder embedBuilder = null)
+                                                            IDiscordServerLogBuilder embedBuilder)
         {
-            await LogToServerAsync(@this, DiscordServerLogLevel.Information, serverConfig, message, embedBuilder);
+            await LogToServerAsync(@this, DiscordServerLogLevel.Information, serverConfig, embedBuilder);
         }
 
         public static async Task LogErrorToServerAsync(this SocketGuild @this,
                                                        DiscordServerConfiguration serverConfig,
-                                                       string message = "", EmbedBuilder embedBuilder = null)
+                                                       IDiscordServerLogBuilder embedBuilder)
         {
-            await LogToServerAsync(@this, DiscordServerLogLevel.Error, serverConfig, message, embedBuilder);
+            await LogToServerAsync(@this, DiscordServerLogLevel.Error, serverConfig, embedBuilder);
         }
     }
 }
