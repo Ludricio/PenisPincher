0e1ed933e5add539ab25080a2e027dd32a1fff56
Ludvig Rydahl <ludvig.rydahl@gmail.com>
2021-04-12 19:24:30 +0200

    Added model mappings

---
+++ PenisPincher.Persistence/Configuration/Mappings/MonitoredStreamMapping.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Text.Json;
+using Microsoft.EntityFrameworkCore;
+using PenisPincher.Core.Models;
+
+namespace PenisPincher.Persistence.Configuration.Mappings
+{
+    internal class MonitoredStreamMapping : EntityConfiguration<MonitoredStream>
+    {
+        protected override void Configure(EntityConfigurator<MonitoredStream> configurator)
+        {
+            configurator
+                .Has(e => e.HasOne(p => p.OwningServer).WithMany(p => p.MonitoredStreams)
+                    .OnDelete(DeleteBehavior.Cascade))
+                .Has(e => e.Property(p => p.NotificationTemplate).IsRequired())
+                .Has(e => e.Property(p => p.StreamName))
+                .Has(e => e.Property(p => p.NotificationRoleIds).HasConversion(
+                    v => JsonSerializer.Serialize(v, null),
+                    v => JsonSerializer.Deserialize<List<ulong>>(v, null)));
+        }
+    }
+}
---
+++ PenisPincher.Persistence/Configuration/Mappings/ReactionRoleMapping.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PenisPincher.Core.Models;
+
+namespace PenisPincher.Persistence.Configuration.Mappings
+{
+    internal class ReactionRoleMapping : EntityConfiguration<ReactionRole>
+    {
+        protected override void Configure(EntityConfigurator<ReactionRole> configurator)
+        {
+            configurator
+                .Has(e => e.HasOne(p => p.OwningServer).WithMany(p => p.RoleReactions).OnDelete(DeleteBehavior.Cascade))
+                .Has(e => e.Property(p => p.EmoteName).IsRequired())
+                .Has(e => e.Property(p => p.RoleId).IsRequired())
+                .Has(e => e.Property(p => p.MessageId).IsRequired())
+                .Has(e => e.Property(p => p.ChannelId).IsRequired());
+        }
+    }
+}
