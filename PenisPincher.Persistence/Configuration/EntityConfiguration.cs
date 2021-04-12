c44fa7475eddaed966791a55fda60667059b2902
Ludvig Rydahl <ludvig.rydahl@gmail.com>
2021-04-12 18:52:44 +0200

    Added default Entity mappings

--- PenisPincher.Persistence/Configuration/EntityConfiguration.cs
+++ PenisPincher.Persistence/Configuration/EntityConfiguration.cs
@@ -5,14 +5,21 @@ using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using PenisPincher.Core;
 
 namespace PenisPincher.Persistence.Configuration
 {
-    public abstract class EntityConfiguration<TEntity> : IEntityTypeConfiguration<TEntity> where TEntity : class
+    public abstract class EntityConfiguration<TEntity> : IEntityTypeConfiguration<TEntity> where TEntity : class, IEntity
     {
         public void Configure(EntityTypeBuilder<TEntity> builder)
         {
-
+            builder.HasKey(p => p.Id);
+            builder.Property(p => p.CreatedBy).IsRequired();
+            builder.Property(p => p.CreatedDate).IsRequired();
+            builder.Property(p => p.ModifiedBy).IsRequired();
+            builder.Property(p => p.ModifiedDate).IsRequired();
+            builder.Property(p => p.Version).IsRequired();
+            Configure(new EntityConfigurator<TEntity>(builder));
         }
 
         protected abstract void Configure(EntityConfigurator<TEntity> configurator);
