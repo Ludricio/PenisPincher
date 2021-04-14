bf37dc965739ac4c55f6d061c226dc2acbb01694
Ludvig Rydahl <ludvig.rydahl@gmail.com>
2021-04-14 12:08:47 +0200

    Added db migration on launch. Refactored over dbcontext provider configuration. Cleaned up and removed unused code

--- PenisPincher.Discord/Bot.cs
+++ PenisPincher.Discord/Bot.cs
@@ -1,8 +1,8 @@
 ﻿using System;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using NLog.Extensions.Logging;
 using PenisPincher.Persistence;
@@ -12,9 +12,6 @@ namespace PenisPincher.Discord
 {
     public class Bot
     {
-        private IConfigurationRoot Configuration { get; }
-        private ILogger<Bot> Logger { get; set; }
-
         private Bot()
         {
             Configuration = new ConfigurationBuilder()
@@ -23,28 +20,34 @@ namespace PenisPincher.Discord
                 .Build();
         }
 
-        public static async Task RunAsync() => await new Bot().Run();
+        private IConfigurationRoot Configuration { get; }
+        private ILogger<Bot> Logger { get; set; }
+
+        public static async Task RunAsync()
+        {
+            await new Bot().Run();
+        }
 
         private async Task Run()
         {
             var services = new ServiceCollection();
             ConfigureServices(services);
-            
+
 
             await using var provider = services.BuildServiceProvider();
             Logger = provider.GetRequiredService<ILogger<Bot>>();
             Logger.Information("All services configured, begin initialization");
 
+            await provider.GetRequiredService<DbContext>().Database.MigrateAsync();
+
             try
             {
-                EnsureDatabaseValidity(provider);
-
                 await using var launcher = provider.GetRequiredService<DiscordLauncher>();
 
                 Logger.Information("Entering async wait loop, event reaction enabled.");
                 await Task.Delay(-1);
             }
-            catch(Exception e)
+            catch (Exception e)
             {
                 Logger.Critical(e, "Uncaught exception");
                 throw;
@@ -53,7 +56,10 @@ namespace PenisPincher.Discord
 
         private void ConfigureServices(IServiceCollection services)
         {
-            services.AddLogging(builder =>
+            services
+                .AddDbContext<DbContext, PenisPincherDbContext>(options =>
+                    options.UseSqlServer(Configuration.ConnectionString()))
+                .AddLogging(builder =>
                 {
                     builder.ClearProviders();
 #if DEBUG
@@ -63,19 +69,10 @@ namespace PenisPincher.Discord
 #endif
                     builder.AddNLog(Configuration);
                 })
-                .AddPersistence(Configuration.ConnectionString())
                 .AddDiscordServices()
                 .AddSingleton<Random>()
                 .AddSingleton(Configuration)
                 .AddSingleton<DiscordLauncher>();
-        }
-
-        private async void EnsureDatabaseValidity(IServiceProvider provider)
-        {
-            await using var context = provider.GetRequiredService<DbContext>();
-            Logger.Information("Ensuring database validity...");
-            await context.Database.EnsureCreatedAsync();
-            Logger.Information("Database valid");
         }
     }
 }
