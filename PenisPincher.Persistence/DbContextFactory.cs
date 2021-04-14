538858f4335cb06afd3a2952ba9c082d48e6e378
Ludvig Rydahl <ludvig.rydahl@gmail.com>
2021-04-14 12:05:38 +0200

    Added designtimedbcontextfactory

---
+++ PenisPincher.Persistence/DbContextFactory.cs
@@ -0,0 +1,21 @@
+﻿using System.IO;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace PenisPincher.Persistence
+{
+    public class DbContextFactory : IDesignTimeDbContextFactory<PenisPincherDbContext>
+    {
+        public PenisPincherDbContext CreateDbContext(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddYamlFile("dbconfig.yaml")
+                .Build();
+            var builder = new DbContextOptionsBuilder<PenisPincherDbContext>();
+            builder.UseSqlServer(configuration["database:connection_string"]);
+            return new PenisPincherDbContext(builder.Options);
+        }
+    }
+}
